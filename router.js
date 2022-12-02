406cef7c4aff0f0c67a6ed1ed61ff468af205b0c
demog <dg08gg@mail.ru>
2022-12-02 22:15:12 +0300

    router.js / обработка запросов
    Добавил обработку запросов на GET и POST запрос на /tasks

--- router.js
+++ router.js
@@ -10,4 +10,12 @@ router.post('/user/sessions', (req, res) => {
   mongoHandler.login(req.body.username, req.body.password, res).then()
 })
 
+router.get('/tasks', (req, res) => [
+  mongoHandler.findTasks(req.get('Token'), req.body.userId, res)
+])
+
+router.post('/tasks', (req, res) => {
+  mongoHandler.insertTask(req.get('Token'), req.body.userId, req.body.title, req.body.description, res).then()
+})
+
 module.exports = router
