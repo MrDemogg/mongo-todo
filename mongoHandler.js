7864e56173d469499c0be2c76c3d1705f858820b
demog <dg08gg@mail.ru>
2022-12-02 21:21:43 +0300

    mongoHandler.js / взаимодействие с mongoDb
    Изменил проверку на уже существующего пользователя в insertUser и добавил функцию для login

--- mongoHandler.js
+++ mongoHandler.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt')
 const {MongoClient, ObjectId} = require('mongodb')
+const {v4} = require('uuid')
 
 const client = new MongoClient('mongodb://localhost:27017')
 
@@ -8,7 +9,15 @@ const mongoHandler = {
     try {
       await client.connect()
       const collection = await client.db('todo').collection('users')
-      if (username === await collection.findOne({username: username})) {
+      let isProfileExists = false
+      await collection.findOne({username: username}).then(response => {
+        if (response) {
+          if (username === response.username) {
+            isProfileExists = true
+          }
+        }
+      })
+      if (isProfileExists) {
         res.status(403).send('Пользователь с таким username уже существует')
       } else {
         let securePassword
@@ -17,6 +26,38 @@ const mongoHandler = {
         });
         await collection.insertOne({username: username, password: securePassword, _id: new ObjectId()}).then()
         res.status(201).send('Успешно')
+      }
+      await client.close()
+    } catch (e) {
+      res.status(500).send('Ошибка')
+    }
+  },
+  login: async (username, password, res) => {
+    try {
+      await client.connect()
+      const collection = await client.db('todo').collection('users')
+      let token = v4()
+      while (token === await collection.findOne({token: token})) {
+        token = v4()
+      }
+      let profile = await collection.findOne({username: username})
+      let isPasswordCorrect = false
+      if (profile !== null) {
+        await bcrypt.compare(password, profile.password).then(function (result) {
+          isPasswordCorrect = result
+        })
+        if (isPasswordCorrect) {
+          await collection.updateOne(
+            {username: username, password: profile.password},
+            {$set: {token: token}}).then(() => {
+              res.status(201).send({token: token})
+            }
+          )
+        } else {
+          res.status(418).send('Неправильный пароль пупсик')
+        }
+      } else {
+        res.status(404).send('Пользователь не найден')
       }
       await client.close()
     } catch (e) {
